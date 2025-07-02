5fc339a702ca3b4da7d2f86ddefdf99c27088fe3
ingarskrafts <ingarskrafts@gmail.com>
2025-07-02 07:22:14 -0400

    step5 started

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -3,7 +3,14 @@ const CartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        
+        addItemToCart(state, action) {
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
     }
 });
 
