bde4fda747b4ea2b275279a7c20a12de60993b1d
ingarskrafts <ingarskrafts@gmail.com>
2025-07-03 06:13:52 -0400

    Step 5: Reducers creation and export actions

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -11,9 +11,36 @@ const CartSlice = createSlice({
                 state.cartItems.push({ ...action.payload, quantity: 1 });
             }
         },
+        removeItemFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        },
+        clearCart(state) {
+            state.cartItems = [];
+        },
+        increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
     }
 });
 
 const initialState = {
     cartItems: [],
 };
+
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
