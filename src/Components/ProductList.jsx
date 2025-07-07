e67b022515eb9f85cf85d13abcc4aca38648d79d
ingarskrafts <ingarskrafts@gmail.com>
2025-07-07 06:06:25 -0400

    step7 started

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice'; // Action to add product to cart
+
+const dispatch = useDispatch();
+const cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
 
 const ProductList = () => {
 
@@ -16,8 +21,12 @@ const ProductList = () => {
         {products.map(product => (
             <li key={product.id} className="product-list-item">
                 <span>{product.name} - ${product.price}</span>
-                <button>
-                    Add to Cart
+                <button
+                    className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? 'disabled' : ''}`}
+                    onClick={() => handleAddToCart(product)}
+                    disabled={cartItems.some(item => item.id === product.id)} // Disable if already in cart
+                >
+                    {cartItems.some(item => item.id === product.id) ? 'Added' : 'Add to Cart'}
                 </button>
             </li>
         ))}
